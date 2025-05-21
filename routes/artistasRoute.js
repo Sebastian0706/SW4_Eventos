5c54aeb962a23dfb80bf8c8585839d3c8ba87321
Angelica <aglc97@hotmail.com>
2025-05-21 12:08:00 -0500

    feat: agregar validación en POST y PUT para que no se repitan artistas

--- routes/artistasRoute.js
+++ routes/artistasRoute.js
@@ -6,7 +6,7 @@ const validateArtistas = require('../middlewares/artistaValidate');
 router.get('/', artistasController.index);
 router.get('/create', artistasController.create);
 router.post('/', validateArtistas, artistasController.store);
-router.get('/:id_artista/editar', artistasController.edit);
+router.get('/:id_artista/edit', artistasController.edit);
 router.put('/:id_artista', validateArtistas, artistasController.update);
 router.delete('/:id_artista', artistasController.delete);
 router.get('/:id_artista', artistasController.show);
